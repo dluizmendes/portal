d1f618ddac4976cf75710d12d128c64cc7995682
Douglas L Mendes <dluizmendes@gmail.com>
2026-01-18 22:22:05 -0300

    fixing some issues during build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Expertise from './components/Expertise'
-import Certifications from './components/Certifications'
-import CurrentWork from './components/CurrentWork'
-import Experience from './components/Experience'
-import Skills from './components/Skills'
-import Education from './components/Education'
-import Contact from './components/Contact'
 
 export const metadata: Metadata = {
   title: 'Douglas Mendes - SRE',
